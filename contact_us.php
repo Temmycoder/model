618c804649d35cb7041a00504bcb1e7970965c13
Temmycoder <oladutemutemiloluwa@icloud.com>
2025-02-21 11:51:18 +0100

    new features were added at this point

--- contact_us.php
+++ contact_us.php
@@ -1,65 +1,58 @@
-<?php
-    include('includes/connect.php');
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
-    require 'PHPMailer/PHPMailer/src/Exception.php';
-    require 'PHPMailer/PHPMailer/src/PHPMailer.php';
-    require 'PHPMailer/PHPMailer/src/SMTP.php';
-    require 'vendor/autoload.php';
-
-    if(isset($_POST['contact'])){
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-
-        $sql = mysqli_query($conn, "SELECT * FROM user_tbl WHERE role = 'admin'");
-        $row = mysqli_fetch_assoc($sql);
-
-    }
-?>
-
 <html>
 <head>
-    <title>Change Password</title>
-    <link rel="stylesheet" type="text/css" href="dashboard.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="css/all.css">
+<title>Contact Us</title>
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="author" content="TemplatesJungle">
+  <meta name="keywords" content="ecommerce,fashion,store">
+  <meta name="description" content="Bootstrap 5 Fashion Store HTML CSS Template">
 
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
     <style>
         .box{
             border-radius: 7px;
             font-size: 20px;
-            margin: 40px 7cm 0;
-            padding: 70px 30px 50px 30px;
+            padding: 40px 30px 30px 30px;
             border: none;
-            background-color: #ddd;
+            background-color: #eee;
             text-align: left;
         }
     </style>
 </head>
-<body>
+<body class="mb-5">
     <main>
         <?php include('includes/header.php');?>
 
-        <h1 class="pt-5 text-center">Change Password</h1>
-            <div class="box">
-                <?php echo $error_msg; echo $success?>
+        <div class="row mt-5">
+            <div class="col-md-3"></div>
+
+            <div class="box col-md-6">
+            <h4 class="text-center">Contact Us</h4>
+
                 <form method="post">
                     <label>Name:</label>
                     <input type="text" name="name" id="name" class="form-control" required><br>
                     <label>Email:</label>
                     <input type="email" name="email" id="email" class="form-control" required><br>
-                    <label>Phone Number:</label>
-                    <input type="tel" name="phone" id="phone" maxlength="16" minlength="8" class="form-control" required><br><br>
-                    <div class="text-center">
+                    <label>Message:</label>
+                    <textarea rows="5" name="text" class="form-control">Hello, </textarea>
+                    <div class="text-center mt-5">
                         <input type="submit" name="contact" value="Submit" class="btn btn-primary">
                     </div>
                 </form>
             </div>
+            <div class="col-md-3"></div>
+        </div>
     </main>
 
-    <script type="text/javascript" src="js/all.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/all.js"></script>
+    <script src="js/script.min.js"></script>
 </body>
 </html>
---
+++ includes/footer.php
@@ -0,0 +1,132 @@
+<footer id="footer" class="mt-5">
+  <div class="container">
+    <div class="row d-flex flex-wrap justify-content-between py-5">
+      <div class="col-md-3 col-sm-6" data-aos="fade-down" data-aos-delay="0">
+        <div class="footer-menu footer-menu-001">
+          <div class="footer-intro mb-4">
+            <a href="index.php">
+              <img src="images/main-logo.png" alt="logo">
+            </a>
+          </div>
+          <p>Follow our social media to stay updated on the MODELLA media modelling contests.</p>
+          <div class="social-links">
+            <ul class="list-unstyled d-flex flex-wrap gap-3">
+              <li>
+                <a href="#" class="text-secondary">
+                  <svg width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#facebook"></use>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="text-secondary">
+                  <svg width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#twitter"></use>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="text-secondary">
+                  <svg width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#youtube"></use>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="text-secondary">
+                  <svg width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#pinterest"></use>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a href="#" class="text-secondary">
+                  <svg width="24" height="24" viewBox="0 0 24 24">
+                    <use xlink:href="#instagram"></use>
+                  </svg>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6" data-aos="fade-down" data-aos-delay="300">
+        <div class="footer-menu footer-menu-002">
+          <h5 class="widget-title text-uppercase mb-4">Quick Links</h5>
+          <ul class="menu-list list-unstyled text-uppercase border-animation-left fs-6">
+            <li class="menu-item">
+              <a href="index.php" class="item-anchor">Home</a>
+            </li>
+            <li class="menu-item">
+              <a href="#" class="item-anchor">About</a>
+            </li>
+            <li class="menu-item">
+              <a href="#" class="item-anchor">Services</a>
+            </li>
+            <li class="menu-item">
+              <a href="contestant.php" class="item-anchor">Models</a>
+            </li>
+            <li class="menu-item">
+              <a href="contact_us.php" class="item-anchor">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6" data-aos="fade-down" data-aos-delay="600">
+        <div class="footer-menu footer-menu-003">
+          <h5 class="widget-title text-uppercase mb-4">Help & Info</h5>
+          <ul class="menu-list list-unstyled text-uppercase border-animation-left fs-6">
+            <li class="menu-item">
+              <a href="#" class="item-anchor">Track A Model Status</a>
+            </li>
+            <li class="menu-item">
+              <a href="#" class="item-anchor">Details</a>
+            </li>
+            <li class="menu-item">
+              <a href="#" class="item-anchor">Contact Us</a>
+            </li>
+            <li class="menu-item">
+              <a href="#" class="item-anchor">Media</a>
+            </li>
+            <li class="menu-item">
+              <a href="index.html" class="item-anchor">Faqs</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6" data-aos="fade-down" data-aos-delay="900">
+        <div class="footer-menu footer-menu-004 border-animation-left">
+          <h5 class="widget-title text-uppercase mb-4">Contact Us</h5>
+          <p>Do you have any questions or suggestions? <a href="mailto:contact@yourcompany.com"
+              class="item-anchor">contact@yourcompany.com</a></p>
+          <p>Do you need support? Give us a call. <a href="tel:+43 720 11 52 78" class="item-anchor">+234 706 6246 499</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="border-top py-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 d-flex flex-wrap">
+          <div class="shipping">
+            <span>We Host with:</span>
+            <img src="images/arct-icon.png" alt="icon">
+            <img src="images/dhl-logo.png" alt="icon">
+          </div>
+          <div class="payment-option">
+            <span>Payment Option:</span>
+            <img src="images/visa-card.png" alt="card">
+            <img src="images/paypal-card.png" alt="card">
+            <img src="images/master-card.png" alt="card">
+          </div>
+        </div>
+        <div class="col-md-6 text-md-end">
+          <p>© Copyright 2022 MODELLA. All rights reserved. <br>Design by <a href="https://github.com/temmycoder"
+              target="_blank">TemmyCoder</a> Distribution By <a href="https://github.com/temmycoder"
+            target="blank">AceTeam</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
